50b3dcd938312de33ea0dfed145eb0b7b8532fd9
p.troshkin <troshkin.pavel@yandex.ru>
2015-07-14 11:56:16 +0500

    Android mobile configuration

--- out/app.js
+++ out/app.js
@@ -29,7 +29,7 @@ InfinniUI.config.lang = 'ru-RU';
 InfinniUI.config.maxLengthUrl = 2048;
 InfinniUI.config.cacheMetadata = false; //boolean - enable/disable cache | milliseconds
 InfinniUI.config.serverUrl = 'http://10.222.222.68:9900';
-InfinniUI.config.configId = 'PTA';
+InfinniUI.config.configId = 'PTA_mobile';
 InfinniUI.config.configName = 'Родительский коммитет';
 /**
  * Набор утилит для работы с BlobData объектами
